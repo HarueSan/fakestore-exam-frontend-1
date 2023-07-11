cc25b0f39da409e3f2200efd4a3102e2f99e63cc
Haruethai Sangyod <haruethai.sa@inet.co.th>
2023-07-11 23:30:53 +0700

    first commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
-createApp(App).mount('#app')
+import store from './store'
+import router from './router'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+const app = createApp(App)
+
+app.use(vuetify)
+app.use(store)
+app.use(router)
+
+app.mount("#app");
